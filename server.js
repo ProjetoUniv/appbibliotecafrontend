641801334b8dfd7d7d8aadd372a02d3dd9f4d47d
aslac2020 <andredesenvolvedorti@gmail.com>
2021-10-30 23:21:56 -0300

    alterações

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8082;
 
 app.use(express.static(__dirname + '/dist/controleLivros'));
 
