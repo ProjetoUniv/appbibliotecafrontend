5a3b8dda17b535d0a831533986bb3641c75b00bf
aslac2020 <andredesenvolvedorti@gmail.com>
2021-10-30 23:39:55 -0300

    alterações

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 const PORT = process.env.PORT || 8082;
 
 app.use(express.static(__dirname + '/dist/controleLivros'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/dist/controleLivros/index.html');
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/controleLivros/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log('Servidor iniciado na porta' + PORT);
-})
+app.listen(process.env.PORT || 8083);
+
 
 
 
